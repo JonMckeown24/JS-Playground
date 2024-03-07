53824b550a74227247f8a9a6fb162dbfad80fdbb
JonMcKeown24 <jonathan-mckeown@live.nmit.ac.nz>
2024-03-07 14:58:57 +1300

    added logic statements

--- numbers.js
+++ numbers.js
@@ -1,11 +1,41 @@
 /*NOTE - Const (Variable keyword) num2 (Variable name) = 10 (Variable assignment)
 Const is a declaration of a variable that cannot be changed 
 let and var are declarations that can be changed*/
-const num1 = 10
-console.log (num1)
+// const pi = 3.14
+// //pi = 10
+// console.log (pi)
 
-let num2 = 20
-console.log(num2)
+// let num2 = 20
+// num2 = 25
+// console.log(num2)
 
-var num3 = 30
-console.log (num3)
+// var num3 = 30
+// console.log (num3)
+
+// const fullname = 'Jon McKeown'
+// console.log (fullname)
+
+// const workplace = 'NMIT'
+// console.log (workplace)
+
+// console.log (fullname + " " + workplace)
+
+// console.log (`my full name is ${fullname} and I work at ${workplace}`)
+
+// const working = true
+// const notworking = false
+
+// if (pi == 3.14) {
+//     console.log (fullname)
+// }
+
+let a = 0
+let result
+// if () {} else {}
+  if (a >= 0) {
+    result = 'positive'
+    console.log (result)
+  } else {
+    result = 'NOT positive'
+    console.log (result)
+  }
