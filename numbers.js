8c9b6bc7a6686c564c902353c7ad4b33fcb27c55
JonMcKeown24 <jonathan-mckeown@live.nmit.ac.nz>
2024-03-07 13:54:33 +1300

    added numbers.js file describing const, let and var

---
+++ numbers.js
@@ -0,0 +1,11 @@
+/*NOTE - Const (Variable keyword) num2 (Variable name) = 10 (Variable assignment)
+Const is a declaration of a variable that cannot be changed 
+let and var are declarations that can be changed*/
+const num1 = 10
+console.log (num1)
+
+let num2 = 20
+console.log(num2)
+
+var num3 = 30
+console.log (num3)
