cbecb00a7de2b5218dc1c3e9df90c9676ab8f8ac
JonMcKeown24 <jonathan-mckeown@live.nmit.ac.nz>
2024-03-19 15:44:31 +1300

    added duplicates in an array

--- functions-2.js
+++ functions-2.js
@@ -53,8 +53,56 @@ function sumMinArrays(arr) {
 
 const sumMinArrayNew = arr => arr.reduce((a,b) => a +Math.min(...b),0)
 
-console.log(sumMinArrays([
+/*console.log(sumMinArrays([
     [1,2,3,4,5],
     [6,7,8,9,10],
     [11,12,13,14,15]
-]))
+]))*/
+
+/* create a fuction that takes an array, return duplicate numbers in an array.
+ArraysDuplicateNumbers ([1,2,3,4,5,6,5,1]) output -> [5,1] */
+
+function findDuplicateNumbers(arr) {
+    const duplicates = [];
+    const seen = {};
+
+    for (const num of arr) {
+        if (seen[num]) {
+            duplicates.push(num);
+        } else {
+            seen[num] = true;
+        }
+    }
+
+    return duplicates;
+}
+
+//console.log(findDuplicateNumbers([1, 2, 3, 4, 5, 6, 5, 1]))
+
+const findDuplicateNumbers2 = arr => [...new Set(arr.filter((value, index, array) => 
+    array.indexOf(value) !== index))];
+
+//console.log(findDuplicateNumbers2([1, 2, 3, 4, 5, 6, 5, 1, 2, 5]));
+
+const ArraysDuplicateNumbers = arr => (
+    arr= [... new Set([...arr.filter(num =>
+        arr.indexOf(num) !== arr.lastIndexOf(num)
+        )])].sort((a,b) => a-b),
+        arr.length ? arr: null
+)
+
+//console.log(ArraysDuplicateNumbers([1, 2, 3, 4, 5, 6, 5, 1]))
+
+var ArraysDuplicateNumbers2 = function(arr) {
+    arr = arr.filter(function(num, index, array) {
+        return array.indexOf(num) !== array.lastIndexOf(num);
+    });
+    arr = Array.from(new Set(arr));
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+    return arr.length ? arr : null;
+};
+
+console.log(ArraysDuplicateNumbers2([1, 2, 3, 4, 5, 6, 5, 1]))
+
